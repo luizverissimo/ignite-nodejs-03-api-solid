1874a1aad42cb741015845e2c92baef3fa1c37a6
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-02-22 10:44:53 -0300

    casos de uso procurar uma academia, pegar numero de checkins do usuario, e buscas academias proximas

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,12 +1,12 @@
 import { CheckIn, Prisma } from '@prisma/client'
-import { CheckInsRepository } from '../check-ins-repository'
-import { randomUUID } from 'node:crypto'
 import dayjs from 'dayjs'
+import { randomUUID } from 'node:crypto'
+import { CheckInsRepository } from '../check-ins-repository'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
-  async create(data: Prisma.CheckInUncheckedCreateInput) {
+  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn = {
       id: randomUUID(),
       user_id: data.user_id,
@@ -39,5 +39,15 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     if (!checkInOnSameDate) return null
 
     return checkInOnSameDate
+  }
+
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
+
+  async countByUserId(userId: string): Promise<number> {
+    return this.items.filter((item) => item.user_id === userId).length
   }
 }
---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,56 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { SearchGymUseCase } from './search-gyms'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: SearchGymUseCase
+
+describe('Search Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new SearchGymUseCase(gymsRepository)
+  })
+
+  it('should be able to search for gyms', async () => {
+    await gymsRepository.create({
+      title: 'Javascript gym',
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    })
+
+    await gymsRepository.create({
+      title: 'Typescript gym',
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    })
+
+    const { gyms } = await sut.execute({ query: 'Javascript', page: 1 })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Javascript gym' })])
+  })
+
+  it('should be able to fetch a paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `gym-${i}`,
+        description: null,
+        phone: null,
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+    }
+
+    const { gyms } = await sut.execute({ query: 'gym', page: 2 })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'gym-21' }),
+      expect.objectContaining({ title: 'gym-22' }),
+    ])
+  })
+})
