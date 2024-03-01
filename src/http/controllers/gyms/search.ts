6ebf380c4f9dc590a59572e915fc73d5d3a61108
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-03-01 09:56:50 -0300

    criando controllers

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,19 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+  const { q, page } = searchQuerySchema.parse(request.query)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+  const { gyms } = await searchGymsUseCase.execute({
+    query: q,
+    page,
+  })
+
+  return reply.status(201).send({ gyms })
+}
